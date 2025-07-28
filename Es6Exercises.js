0aa3f2263a50eba95c8dfc88547d2021fd93f585
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-28 19:52:03 +0200

    updation

--- Es6Exercises.js
+++ Es6Exercises.js
@@ -9,3 +9,8 @@ const multEachArrayEle=(array)=>{
 
 console.log(multEachArrayEle([1,3,4,5,6]))
 
+// Template literals challenge give an {name: 'samue', age:25},return string //"My name is Samuel and I am 25 years old"
+
+
+const template={name:'samuel',age:25}
+console.log(`My name is ${template.name} and I am ${template.age} year `)
