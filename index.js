959c119f48103a68106ae99a7c6ba70fd7668564
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-28 15:23:01 +0200

    updation

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ function greet([first , second]){
 }
 greet([300, 400, 450])
 greet([300, 400, 450])
+
+// for...of loop it iterates over values of iterable(like array, string , maps ,set etc)
+
+// for array nested
+
+
+const matrix=[[1,2],[3,4],[5,6]]
+
+for(const arr of matrix){
+    for(const value of arr){
+        console.log(value)
+
+    }
+
+}
+
+   
