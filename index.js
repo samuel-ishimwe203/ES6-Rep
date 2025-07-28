2509c10649ee7577ce9161b601167825d0a13cce
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-28 17:01:02 +0200

    updation

--- index.js
+++ index.js
@@ -38,4 +38,22 @@ function sum(){
 console.log(sum(1,2,3,4,5,6))
 
 
+
+function negPos(arr){
+    const array=arr.filter(num=>num>0)
+    const arra=arr.filter(num=>num<0)
+    
+    const sumOfPos=array.reduce((acc,curr)=>acc+curr,-1)
+    const subtractNeg=arra.reduce((acc,curr)=>acc*curr)
+    
+    const finalResult= [sumOfPos,subtractNeg]
+    
+    return finalResult
+    
+    
+}
+
+console.log(negPos([-3,-4,-2,1,3,4]))
+
+
    
