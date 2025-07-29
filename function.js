6aa769910e7a2492fb0e9ac677e351c2a1eba4bc
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-29 11:21:27 +0200

    function updation

--- function.js
+++ function.js
@@ -8,4 +8,22 @@ const defaul= (name='John doe',Greeting='Welcome')=>{
 console.log(defaul())
 console.log(defaul('bite se','mucyo'))
 
+// nested object destructuring
+
+
+const user ={
+    name:'samuel',
+    score:{
+        math:50 ,
+         Englis:70
+        }
+}
+
+const {
+    score:{math ,Englis}
+} = user
+
+console.log(math)
+console.log(Englis)
+
 
