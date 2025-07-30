674c1376e41b69266ec8d597f8fc0e14956e58cc
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-30 09:42:05 +0200

    something changes

--- codewars.js
+++ codewars.js
@@ -6,3 +6,69 @@ function reverseWords(str){
 }
 
 console.log(reverseWords('hello world my ghee'))
+
+
+// Calculating with Functions
+
+function zero(number) {
+  return number? number(0):0
+}
+function one(number) {
+    return number? number(1):1
+}
+function two(number) {
+    return number? number(2):2
+}
+function three(number) {
+    return number? number(3):3
+}
+function four(number) {
+    return number? number(4):4
+}
+function five(number) {
+     return number? number(5):5
+}
+function six(number) {
+    return number? number(6):6
+}
+function seven(number) {
+    return number? number(7):7
+}
+function eight(number) {
+    return number? number(8):8
+}
+function nine(number) {
+    return number? number(9):9
+}
+
+
+
+function plus(num1) {
+    return function(num2){
+        return num2+num1
+    }
+   
+}
+function minus(num1) {
+    return function(num2){
+        return num2-num1
+    }
+   
+}
+
+
+function times(num1) {
+    return function(num2){
+        return num2*num1
+    }
+   
+}
+
+function dividedBy(num1) {
+    return function(num2){
+        return Math.floor(num2/num1)
+    }
+   
+}
+
+console.log(seven(dividedBy(five())))
